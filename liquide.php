d837535a2fe865a9472d2d12a22419e0f3243cf6
julie <perianmodely.julie@gmail.com>
2022-03-29 18:57:06 +0400

    modif url

--- liquide.php
+++ liquide.php
@@ -56,7 +56,7 @@
 
 
     <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=vap_store', 'root', '');
+    $bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06', 'sc1lgvu9627', 'AFCPE-DWWM#2021-RUN');
 
     $recupliquide = $bdd->query('SELECT * FROM `e-liquide`');
 
