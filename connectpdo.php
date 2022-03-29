08325b39a3f980373602b782fe5408ec1573fd2c
julie <perianmodely.julie@gmail.com>
2022-03-29 20:06:59 +0400

    dalete htacesse

--- connectpdo.php
+++ connectpdo.php
@@ -1,4 +1,7 @@
 
 <?php
-$bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06', 'sc1lgvu9627', 'AFCPE-DWWM#2021-RUN');
+
+if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+    $bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
+}
 ?>
