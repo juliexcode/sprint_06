f353f364ae55fa3cd24e03631ca19841063cc29b
julie <perianmodely.julie@gmail.com>
2022-03-30 10:22:33 +0400

    update

--- connectpdo.php
+++ connectpdo.php
@@ -1,7 +1,21 @@
 
 <?php
 
-if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
-    $bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
-}
-?>
+try {
+    // dev configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+        $bdd = new PDO(
+            'mysql:host=localhost;dbname=vap_store;charset=utf8',
+            'root',
+            ''
+        );
+    }
+
+    // prod configuration
+
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+        $bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06;charset=utf8', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
+    }
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+} ?>
--- index.php
+++ index.php
@@ -60,24 +60,7 @@
     </header>
     <h1>Vapoteuse</h1>
     <?php
-    try {
-        // dev configuration
-        if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
-            $bdd = new PDO(
-                'mysql:host=localhost;dbname=vap_store;charset=utf8',
-                'root',
-                ''
-            );
-        }
-
-        // prod configuration
-
-        if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
-            $bdd = new PDO('mysql:host=109.234.164.161;dbname=sc1lgvu9627_perianmodely-julie.sprint-06', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
-        }
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+    include 'connectpdo.php';
     $vap = $bdd->query('SELECT*FROM vapo ORDER BY id DESC');
     if (isset($_GET['envoyer'])) {
         if (isset($_GET['s']) && !empty($_GET['s'])) {
